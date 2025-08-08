94f660c3d600dc97825ff24bf74adfcbea8a2e8c
ryuseibs <beryusei0923212ssyo@aol.com>
2025-08-08 16:22:02 +0900

    example_Lesson_02_010

---
+++ example_Lesson_02_010.js
@@ -0,0 +1,21 @@
+function setup() {
+  let x = 830;
+  switch (x) {
+    case 431:
+      print("431です");
+      break;
+    case 509:
+      print("509です");
+      break;
+    case 830:
+      print("830です");
+      break;
+    default:
+      print("どれでもない");
+  }
+  print("switch文終了");
+}
+
+function draw() {
+  
+}

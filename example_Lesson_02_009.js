9f92f7b032bb359dd3965f7edf329b813a8ac58b
ryuseibs <beryusei0923212ssyo@aol.com>
2025-08-08 16:16:35 +0900

    example_Lesson_02_009

---
+++ example_Lesson_02_009.js
@@ -0,0 +1,15 @@
+function setup() {
+  let x = 830;
+  if (x > 830) {
+    print("830より大きい");
+  } else if (x > 431) {
+    print("830以下だが、431より大きい")
+  } else {
+    print("830以下");
+  }
+  print("if文終わり");
+}
+
+function draw() {
+  
+}

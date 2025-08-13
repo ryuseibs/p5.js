38128b51ead5f6909d1f460b502f8b5ffcdced34
ryuseibs <beryusei0923212ssyo@aol.com>
2025-08-13 11:47:38 +0900

    example_Lesson_03_016

---
+++ example_Lesson_03_016.js
@@ -0,0 +1,19 @@
+let x = 0;
+function setup() {
+  
+}
+
+function draw() {
+  background(255);
+  rect(x, x, 30, 30);
+  x++;
+  if (x > height) {
+    x = 0;
+  }
+}
+
+function mouseClicked() {
+  if (mouseButton == LEFT) {
+    noLoop();
+  }
+}

bf2bd2a0aa3d7f8c4a4b8bb9cec632ad9bc2f8f8
ryuseibs <beryusei0923212ssyo@aol.com>
2025-08-21 11:22:23 +0900

    example_Lesson_07_001

---
+++ example_Lesson_07_001.js
@@ -0,0 +1,13 @@
+let img;
+
+function preload() {
+  img = loadImage("img_bg_pc2.webp");
+}
+function setup() {
+  createCanvas(img.width, img.height);
+  image(img, 0, 0);
+}
+
+function draw() {
+  
+}
